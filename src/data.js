b2b2129f23e12a217f5b258594bfcea9492d0962
anuoye <anuoyebanjodunmi660@gmail.com>
2022-10-01 11:58:34 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,33 @@
+export const products = [
+    {
+      id: 1,
+      img: "https://images.unsplash.com/file-1661973760703-91f50df21c28image?dpr=2&auto=format&fit=crop&w=416&q=60",
+      link: "http://lama.dev",
+    },
+    {
+      id: 2,
+      img: "https://images.unsplash.com/photo-1662670168474-a6b2e31fe136?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+      link: "http://lama.dev",
+    },
+    {
+      id: 3,
+      img: "https://images.unsplash.com/photo-1662658825798-944da7268bfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
+      link: "http://lama.dev",
+    },
+    {
+      id: 4,
+      img: "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y29kZSUyMGdhZ2RldHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+      link: "http://lama.dev",
+    },
+    {
+      id: 5,
+      img: "https://images.unsplash.com/photo-1483817101829-339b08e8d83f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y29kZSUyMGdhZ2RldHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+      link: "http://lama.dev",
+    },
+    {
+      id: 6,
+      img: "https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGNvZGUlMjBnYWdkZXRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+      link: "http://lama.dev",
+    },
+  ];
+  
